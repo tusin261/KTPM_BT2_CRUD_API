8e09a3ebb76f146da212146d988d856f7ea27a76
unknown <thanhtu3885585@gmail.com>
2021-09-14 14:37:55 +0700

    finish controller get all employee

--- src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
+++ src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
@@ -1,4 +1,24 @@
 package com.se.bt2_nguyenthanhtu_18045511.controller;
 
+import com.se.bt2_nguyenthanhtu_18045511.entity.Employee;
+import com.se.bt2_nguyenthanhtu_18045511.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getListEmployee(){
+        System.out.println(employeeService.findAll());
+        return employeeService.findAll();
+    }
 }
