2b997485e9931806669d4d3daa84d39edcaba9da
unknown <thanhtu3885585@gmail.com>
2021-09-14 14:07:41 +0700

    add services

--- src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
+++ src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
@@ -1,4 +1,17 @@
 package com.se.bt2_nguyenthanhtu_18045511.controller;
 
+import com.se.bt2_nguyenthanhtu_18045511.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+
 }
