9c3d5af03c40e7241090df6f4926bc53677c139d
tusin26 <thanhtu3885585@gmail.com>
2021-09-17 10:25:39 +0700

    add profile for dev, production

--- src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
+++ src/main/java/com/se/bt2_nguyenthanhtu_18045511/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.se.bt2_nguyenthanhtu_18045511.controller;
 
+import com.se.bt2_nguyenthanhtu_18045511.entity.Employee;
 import com.se.bt2_nguyenthanhtu_18045511.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -11,6 +13,37 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getListEmployee(){
+        System.out.println(employeeService.findAll());
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable long id){
+        return employeeService.findById(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employee.setId(Long.valueOf(0));
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable long id){
+        employeeService.deleteById(Long.valueOf(id));
+        return "Deleted employee "+ id;
+    }
 
 
 
